d2b34b5fbcbfcb5bca22c5e2877d64ee8fec4125
Oleg Valter <o.a.philippov@gmail.com>
2021-06-12 00:03:18 +0300

    fixed typo in repository name

--- dist/headers.js
+++ dist/headers.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @author          Oleg Valter
 // @description     Enhancements for flagging experience
-// @homepage        https://github.com/userscripters/flag-overcharge#readme
+// @homepage        https://github.com/userscripters/flag-overcharged#readme
 // @name            flag-overcharge
-// @source          git+https://github.com/userscripters/flag-overcharge.git
-// @supportURL      https://github.com/userscripters/flag-overcharge/issues
+// @source          git+https://github.com/userscripters/flag-overcharged.git
+// @supportURL      https://github.com/userscripters/flag-overcharged/issues
 // @version         1.0.0
 // ==/UserScript==
