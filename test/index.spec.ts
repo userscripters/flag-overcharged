1145909aff55b14e6dffaaa441e9f51cb740b318
Oleg Valter <o.a.philippov@gmail.com>
2021-06-12 06:41:50 +0300

    optimized tests & added test for 0 storage quota

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,33 +4,41 @@ import { JSDOM } from "jsdom";
 
 type Area = HTMLTextAreaElement;
 
+const appendDist = async (d: Document) => {
+    const script = await readFile("./dist/modern/index.js", {
+        encoding: "utf-8",
+    });
+    d.body.append(`<script>${script}</script>`);
+};
+
+const dispatchInput = (
+    modal: HTMLElement,
+    text: string,
+    eiv: typeof InputEvent
+) => {
+    const custom = modal.querySelector<Area>("[name='otherText']")!;
+    custom.value = text;
+    const ev = new eiv("input", { bubbles: true });
+    custom.dispatchEvent(ev);
+};
+
+const getPopup = () =>
+    readFile("./test/fixtures/popup.html", { encoding: "utf-8" });
+
 describe("main", () => {
     it("should save input correctly", async () => {
-        const content = await readFile("./test/fixtures/popup.html", {
-            encoding: "utf-8",
-        });
+        const content = await getPopup();
 
         const {
             window: { document, InputEvent },
         } = new JSDOM(content, { runScripts: "dangerously" });
 
-        const script = await readFile("./dist/modern/index.js", {
-            encoding: "utf-8",
-        });
-
-        document.body.append(`<script>${script}</script>`);
-
-        const areaSel = "[name='otherText']";
+        await appendDist(document);
 
         const modal = document.getElementById("popup-flag-post")!;
-        const custom = modal.querySelector<Area>(areaSel)!;
-
         const text = "test value";
 
-        custom.value = text;
-
-        const ev = new InputEvent("input", { bubbles: true });
-        custom.dispatchEvent(ev);
+        dispatchInput(modal, text, InputEvent);
 
         const dolly = <HTMLElement>modal.cloneNode(true);
 
@@ -39,7 +47,33 @@ describe("main", () => {
 
         modal.replaceWith(dolly);
 
-        const { value } = modal.querySelector<Area>(areaSel)!;
+        const { value } = modal.querySelector<Area>("[name='otherText']")!;
+
+        expect(value).to.equal(text);
+    });
+
+    it("should work with storage disabled", async () => {
+        const content = await getPopup();
+
+        const {
+            window: { document, InputEvent },
+        } = new JSDOM(content, { storageQuota: 0, runScripts: "dangerously" });
+
+        await appendDist(document);
+
+        const modal = document.getElementById("popup-flag-post")!;
+        const text = "no quota, but still works";
+
+        dispatchInput(modal, text, InputEvent);
+
+        const dolly = <HTMLElement>modal.cloneNode(true);
+
+        const form = dolly.querySelector("form")!;
+        form.reset();
+
+        modal.replaceWith(dolly);
+
+        const { value } = modal.querySelector<Area>("[name='otherText']")!;
 
         expect(value).to.equal(text);
     });
