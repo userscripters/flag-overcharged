c362b198b862dcb0342839793d94baf4e6df654c
Oleg Valter <o.a.philippov@gmail.com>
2021-06-20 23:21:02 +0300

    regenerated headers with @matches headers

--- dist/headers.js
+++ dist/headers.js
@@ -2,7 +2,14 @@
 // @author          Oleg Valter
 // @description     Enhancements for flagging experience
 // @homepage        https://github.com/userscripters/flag-overcharged#readme
-// @name            flag-overcharge
+// @match           ://*.askubuntu.com/*
+// @match           ://*.mathoverflow.net/*
+// @match           ://*.serverfault.com/*
+// @match           ://*.stackapps.com/*
+// @match           ://*.stackexchange.com/*
+// @match           ://*.stackoverflow.com/*
+// @match           ://*.superuser.com/*
+// @name            flag-overcharged
 // @source          git+https://github.com/userscripters/flag-overcharged.git
 // @supportURL      https://github.com/userscripters/flag-overcharged/issues
 // @version         1.1.0
